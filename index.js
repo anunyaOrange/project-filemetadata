46c79221ac4e3d72257da265bd9736ae9d0368b7
anunyaOrange <anunyaOrange@proton.me>
2025-06-17 14:58:21 +0700

    testable

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 require('dotenv').config()
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,7 +14,25 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function (req, res) {
+  upload.single('upfile')(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ error: 'File upload failed' });
+    }
+    
+    if (!req.file) {
+      return res.status(400).send({ error: 'No file uploaded' });
+    }
 
+    const fileInfo = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    };
+
+    res.json(fileInfo);
+  });
+});
 
 
 const port = process.env.PORT || 3000;
